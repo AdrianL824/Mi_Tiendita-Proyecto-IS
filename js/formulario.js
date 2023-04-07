1d2f6490d90ee1e47a83de60e00c3c237fc60644
arnoldcopaflores <arnoldcopa2@gmail.com>
2023-04-07 12:35:44 -0400

    expresiones

--- js/formulario.js
+++ js/formulario.js
@@ -10,3 +10,13 @@ const cleanInput = (formulario) => {
 		}
 	});
 }
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{2,30}$/, // Letras y espacios, pueden llevar acentos.
+	apellidos: /^[a-zA-ZÀ-ÿ\s]{2,50}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	direccion:/^[a-zA-Z0-9.#\/\s]{5,100}$/, // Letras y espacios, pueden llevar acentos.
+	telefono: /^\d{7,8}$/, // 7 a 8 numeros.
+	ci: /^\d{6,8}$/, // 4 a 8 digitos.
+	password: /^.{6,20}$/ // 4 a 12 digitos.
+}
