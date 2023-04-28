b7e4bf6b512301bb5aa2868aa9e8a8a4c385e7f6
arnoldcopaflores <arnoldcopa2@gmail.com>
2023-04-27 21:09:25 -0400

    delete

--- delete.php
+++ delete.php
@@ -4,6 +4,16 @@
   require './helpers/providers.php';
   require './helpers/auth.php';
 
-  
+  sessionValidate();
+
+  $url;
+
+  switch($_GET['type']){
+    case 'session':
+      closeSession();
+      $pop = "Sesion Finalizada";
+      $url = '/login.php?pop=' . $pop;
+    break;
+  }
 
 ?>
