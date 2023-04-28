eb46c20897db947eb8b285fd59b2eac136846ba8
arnoldcopaflores <arnoldcopa2@gmail.com>
2023-04-27 21:20:31 -0400

    login

--- login.php
+++ login.php
@@ -4,4 +4,15 @@ require './config/config.php';
 require './helpers/auth.php';
 
 sessionCloseValidate();
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+  $user = loginUser($_POST);
+  if($user){
+    $_SESSION["user"] = $user;
+    header('location: '. RAIZ . "/index.php");
+  }else{
+    $pop = "El correo o contraseña son incorrectos";
+    header('location: '. RAIZ . "/login.php?pop=$pop");
+  }
+}
 ?>
