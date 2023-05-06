71652beba21ca32271c9977d580ceab907da0da9
jonatan <jonatanesprellalopez@gmail.com>
2023-05-06 15:38:09 -0400

    Se reconfiguraron los campos de poner un vacio en el proyexyo

--- views/inicio.view.php
+++
@@ -1,9 +0,0 @@
-<?php require 'views/includes/header.php' ?>
-
-<section class="home--image">
-  <h2 class="title-page">Bienvenido a Mi Tiendita!</h2>
-  <img class="banner-img" src="<?=RAIZ?>/assets/img/logito.png" alt="">
-</section>
-
-
-<?php require 'views/includes/footer.php' ?>
