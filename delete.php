3e2dc5cd8a17a96f2f3754163421ddcb7e63b7d0
arnoldcopaflores <arnoldcopa2@gmail.com>
2023-05-23 22:43:22 -0400

    delete

--- delete.php
+++ delete.php
@@ -2,22 +2,11 @@
   require './config/config.php';
   require './helpers/users.php';
   require './helpers/providers.php';
+  require './helpers/products.php';
+  require './helpers/files.php';
   require './helpers/auth.php';
 
   sessionValidate();
-
-  $url;
-
-  switch($_GET['type']){
-    case 'session':
-      closeSession();
-      $pop = "Sesion Finalizada";
-      $url = '/login.php?pop=' . $pop;
-    break;
-  }
-
-
-  header('location: '. RAIZ . $url);
 
   if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $id = isset($_GET['id']) ? (int) $_GET['id'] : "";
@@ -37,7 +26,29 @@
         $pop = "Proveedor eliminado correctamente";
         $url = '/providers.php?pop=' . $pop;
       break;
+
+      case 'products':
+        $product = getProductByID($id);
+        deleteProduct($id);
+
+        deleteFile($product['thumb'], 'products');
+        $pop = "Producto eliminado correctamente";
+        $url = '/products.php?pop=' . $pop;
+      break;
     }
+    header('location: '. RAIZ . $url);
+  }else{
+
+    $url;
+
+    switch($_GET['type']){
+      case 'session':
+        closeSession();
+        $pop = "Sesion Finalizada";
+        $url = '/login.php?pop=' . $pop;
+      break;
+    }
+  
     header('location: '. RAIZ . $url);
   }
 
