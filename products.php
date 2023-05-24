2c68aac5df53fab4a02c036f13ac430ad3569eac
madaiforesmodragon <madaifloresmodragon@gmail.com>
2023-05-24 10:55:10 -0400

    productos corregido

--- products.php
+++ products.php
@@ -1,12 +1,14 @@
 <?php
   require './config/config.php';
-  require './helpers/providers.php';
+  require './helpers/products.php';
+  require './helpers/shops.php';
   require './helpers/users.php';
   require './helpers/auth.php';
 
   sessionValidate();
 
-  $providers = getProviders();
+  $products = getProducts();
   
+  require './views/products.view.php';
 
 ?>
