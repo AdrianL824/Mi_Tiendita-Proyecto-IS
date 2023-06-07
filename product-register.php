104afd4a1d7e972de3626e47f5a2b9d42193b87b
jonatan <jonatanesprellalopez@gmail.com>
2023-06-06 23:55:05 -0400

    correccion de la implementacion del archivo product-register.php

--- product-register.php
+++ product-register.php
@@ -12,7 +12,8 @@
     $_POST['image'] = saveImage($_FILES['imagen'], 'products'); 
     $user_id = (int) $_SESSION['user']['id'];
     createProduct($_POST, $user_id);
-
+    $pop = 'Producto guardado exitosamente';
+    header('location: '. RAIZ . "/products.php?pop=$pop");
     
   }
 
