341d7ff11c7ac2a71f590eafc87dc33a136bc29a
jonatan <jonatanesprellalopez@gmail.com>
2023-06-06 23:51:26 -0400

    establecimiento de las configuraciones basicas

---
+++ product-register.php
@@ -0,0 +1,10 @@
+<?php
+//configuraciones
+  require './config/config.php';
+  require './helpers/products.php';
+  require './helpers/providers.php';
+  require './helpers/files.php';
+  require './helpers/auth.php';
+
+  
+?>
